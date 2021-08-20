9255ff165c95bd059ca4ac35c300e24e6feb4987
Justin Grote <JustinGrote@users.noreply.github.com>
2021-08-20 11:35:19 -0700

    👷‍♂️Change to Calendar Versioning

---
+++ .github/Get-CalendarVersion.ps1
@@ -0,0 +1,33 @@
+#requires -version 7
+using namespace System.Management.Automation
+Set-StrictMode -Version 3
+$ErrorActionPreference = 'Stop'
+
+<#
+.SYNOPSIS
+Generates a CalendarVersion based on the current commit
+#>
+function Get-CalendarVersion {
+	param(
+		[string]$releaseBranchName = 'main'
+	)
+	$date = [DateTime]::Now
+	$month = $date.Month.ToString().PadLeft(2, '0')
+	$year = $date.Year
+	[string]$datePrefix = $month, $year -join '.'
+
+	#This version component is zero-based so it should be 0 for the first release of the month, 1 for the second, etc.
+	$releaseCount = @(& git tag -l "v$DatePrefix*").count
+
+	if ($releaseBranchName -eq [string](git describe --tags)) {
+		return [SemanticVersion]::new($year, $month, $releaseCount)
+	}
+
+	$branchName = $releaseBranchName -eq 'main' ? 'beta' : (git branch --show-current).split('/') | Select-Object -Last 1
+	[int]$commitsSince = @(git log main..HEAD --oneline).count
+	$prereleaseTag = $branchName, $commitsSince.ToString().PadLeft(3, '0') -join '+'
+
+	return [SemanticVersion]::new($year, $month, $releaseCount, $prereleaseTag)
+}
+
+Get-CalendarVersion
