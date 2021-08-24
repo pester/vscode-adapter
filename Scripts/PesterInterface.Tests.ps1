6ffbaadfc491a5fb07a06c2746362e7e40e8488f
Justin Grote <JustinGrote@users.noreply.github.com>
2021-08-23 18:09:46 -0700

    🧪Clean up and reformat PesterInterface Tests

--- Scripts/PesterInterface.Tests.ps1
+++ Scripts/PesterInterface.Tests.ps1
@@ -1,157 +1,161 @@
 Describe 'PesterInterface' {
-	BeforeAll {
-		$SCRIPT:PesterInterface = Resolve-Path "$PSScriptRoot/PesterInterface.ps1"
-		$SCRIPT:testDataPath = Resolve-Path "$PSScriptRoot/../sample"
-		$SCRIPT:Mocks = Resolve-Path $testDataPath/Tests/Mocks
-	}
+  BeforeAll {
+    $SCRIPT:PesterInterface = Resolve-Path "$PSScriptRoot/PesterInterface.ps1"
+    $SCRIPT:testDataPath = Resolve-Path "$PSScriptRoot/../sample"
+    $SCRIPT:Mocks = Resolve-Path $testDataPath/Tests/Mocks
+  }
 
-	Context 'PesterInterface' {
-		BeforeEach {
-			Import-Module Pester -Force
-			$SCRIPT:pipeOutPath = New-Item "Temp:\PesterInterfaceOutput-$(New-Guid).txt" -ItemType File -Force
-		}
-		AfterEach {
-			Remove-Item $SCRIPT:pipeOutPath
-			Remove-Module Pester
-		}
-		It 'Basic.Tests Discovery' {
-			$paths = "$testDataPath/Tests/Basic.Tests.ps1"
-			& $PesterInterface -Path $paths -Discovery -PipeName $PipeOutPath -DryRun
-			Get-Content $PipeOutPath | ConvertFrom-Json | ForEach-Object label | Should -HaveCount 53
-		}
-		It 'Syntax Error' {
-			$paths = "$testDataPath/Tests/SyntaxError.Tests.ps1"
-			& $PesterInterface -Path $paths -Discovery -PipeName $PipeOutPath -DryRun
-			Get-Content $PipeOutPath | ConvertFrom-Json | ForEach-Object label | Should -HaveCount 1
-		}
-	}
+  Context 'PesterInterface' {
+    BeforeEach {
+      Import-Module Pester -Force
+      $SCRIPT:pipeOutPath = New-Item "Temp:\PesterInterfaceOutput-$(New-Guid).txt" -ItemType File -Force
+    }
+    AfterEach {
+      Remove-Item $SCRIPT:pipeOutPath
+      Remove-Module Pester
+    }
+    It 'Basic.Tests Discovery' {
+      $paths = "$testDataPath/Tests/Basic.Tests.ps1"
+      & $PesterInterface -Path $paths -Discovery -PipeName $PipeOutPath -DryRun
+      Get-Content $PipeOutPath | ConvertFrom-Json | ForEach-Object label | Should -HaveCount 57
+    }
+    It 'Syntax Error' {
+      $paths = "$testDataPath/Tests/ContextSyntaxError.Tests.ps1",
+      "$testDataPath/Tests/DescribeSyntaxError.Tests.ps1"
+      & $PesterInterface -Path $paths -Discovery -PipeName $PipeOutPath -DryRun
+      $testResult = Get-Content $PipeOutPath | ConvertFrom-Json
+      $testResult.id | Should -HaveCount 2
+      $testResult | Where-Object id -Match 'Describesyntaxerror' | ForEach-Object error | Should -Match 'Missing closing'
+      $testResult | Where-Object id -Match 'ContextSyntaxError' | ForEach-Object error | Should -Match 'Missing expression'
+    }
+  }
 
-	Context 'New-TestItemId' {
-		BeforeAll {
-			. $PesterInterface -LoadFunctionsOnly
-		}
-		BeforeEach {
-			$SCRIPT:baseMock = [PSCustomObject]@{
-				PSTypeName  = 'Test'
-				Path        = 'Describe', 'Context', 'It'
-				ScriptBlock = @{
-					File = 'C:\Path\To\Pester\File'
-				}
-				Data        = $null
-			}
-		}
-		It 'basic path' {
-			New-TestItemId -AsString $baseMock |
-				Should -Be $($baseMock.ScriptBlock.File, ($baseMock.Path -join '>>') -join '>>')
-		}
-		It 'Array testcase' {
-			$baseMock.Data = @('test')
-			New-TestItemId -AsString $baseMock |
-				Should -Be $($baseMock.ScriptBlock.File, ($baseMock.Path -join '>>'), '_=test' -join '>>')
-		}
-		It 'Hashtable testcase one key' {
-			$baseMock.Data = @{Name = 'Pester' }
-			New-TestItemId -AsString $baseMock |
-				Should -Be $($baseMock.ScriptBlock.File, ($baseMock.Path -join '>>'), 'Name=Pester' -join '>>')
-		}
-		It 'Hashtable testcase multiple key' {
-			$baseMock.Data = @{Name = 'Pester'; Data = 'Something' }
-			New-TestItemId -AsString $baseMock |
-				Should -Be $($baseMock.ScriptBlock.File, ($baseMock.Path -join '>>'), 'Data=Something>>Name=Pester' -join '>>')
-		}
-		It 'Works without file' {
-			$baseMock.Scriptblock.File = $null
-			$baseMock.Data = @{Name = 'Pester'; Data = 'Something' }
-			New-TestItemId -AsString $baseMock |
-				Should -Be $(($baseMock.Path -join '>>'), 'Data=Something>>Name=Pester' -join '>>')
-		}
-		It 'Works with Pester.Block' {
-			$Block = Import-Clixml $Mocks/Block.clixml
-			New-TestItemId $Block -AsString | Should -Be 'Describe Nested Foreach <name>>>Kind=Animal>>Name=giraffe>>Symbol=🦒'
-		}
-	}
+  Context 'New-TestItemId' {
+    BeforeAll {
+      . $PesterInterface -LoadFunctionsOnly
+    }
+    BeforeEach {
+      $SCRIPT:baseMock = [PSCustomObject]@{
+        PSTypeName  = 'Test'
+        Path        = 'Describe', 'Context', 'It'
+        ScriptBlock = @{
+          File = 'C:\Path\To\Pester\File'
+        }
+        Data        = $null
+      }
+    }
+    It 'basic path' {
+      New-TestItemId -AsString $baseMock |
+        Should -Be $($baseMock.ScriptBlock.File, ($baseMock.Path -join '>>') -join '>>')
+    }
+    It 'Array testcase' {
+      $baseMock.Data = @('test')
+      New-TestItemId -AsString $baseMock |
+        Should -Be $($baseMock.ScriptBlock.File, ($baseMock.Path -join '>>'), '_=test' -join '>>')
+    }
+    It 'Hashtable testcase one key' {
+      $baseMock.Data = @{Name = 'Pester' }
+      New-TestItemId -AsString $baseMock |
+        Should -Be $($baseMock.ScriptBlock.File, ($baseMock.Path -join '>>'), 'Name=Pester' -join '>>')
+    }
+    It 'Hashtable testcase multiple key' {
+      $baseMock.Data = @{Name = 'Pester'; Data = 'Something' }
+      New-TestItemId -AsString $baseMock |
+        Should -Be $($baseMock.ScriptBlock.File, ($baseMock.Path -join '>>'), 'Data=Something>>Name=Pester' -join '>>')
+    }
+    It 'Works without file' {
+      $baseMock.Scriptblock.File = $null
+      $baseMock.Data = @{Name = 'Pester'; Data = 'Something' }
+      New-TestItemId -AsString $baseMock |
+        Should -Be $(($baseMock.Path -join '>>'), 'Data=Something>>Name=Pester' -join '>>')
+    }
+    It 'Works with Pester.Block' {
+      $Block = Import-Clixml $Mocks/Block.clixml
+      New-TestItemId $Block -AsString | Should -Be 'Describe Nested Foreach <name>>>Kind=Animal>>Name=giraffe>>Symbol=🦒'
+    }
+  }
 
-	Context 'Expand-TestCaseName' {
-		BeforeAll {
-			. $PesterInterface -LoadFunctionsOnly
-		}
-		BeforeEach {
-			$SCRIPT:baseMock = New-MockObject -Type Pester.Test
-			$BaseMock.Name = 'Pester'
-		}
-		It 'Fails with wrong type' {
-			$fake = [PSCustomObject]@{Name = 'Pester'; PSTypeName = 'NotATest' }
-			{ Expand-TestCaseName -Test $fake } | Should -Throw '*is not a Pester Test or Pester Block*'
-		}
-		It 'Works with Array testcase' {
-			$baseMock.Name = 'Array TestCase <_>'
-			$baseMock.Data = @('pester')
-			Expand-TestCaseName $baseMock | Should -Be 'Array TestCase pester'
-		}
-		It 'Works with Single Hashtable testcase' {
-			$baseMock.Name = 'Array TestCase <Name>'
-			$baseMock.Data = @{Name = 'pester' }
-			Expand-TestCaseName $baseMock | Should -Be 'Array TestCase pester'
-		}
-		It 'Works with Multiple Hashtable testcase' {
-			$baseMock.Name = 'Array <Data> TestCase <Name>'
-			$baseMock.Data = @{Name = 'pester'; Data = 'aCoolTest' }
-			Expand-TestCaseName $baseMock | Should -Be 'Array aCoolTest TestCase pester'
-		}
+  Context 'Expand-TestCaseName' {
+    BeforeAll {
+      . $PesterInterface -LoadFunctionsOnly
+    }
+    BeforeEach {
+      $SCRIPT:baseMock = New-MockObject -Type Pester.Test
+      $BaseMock.Name = 'Pester'
+    }
+    It 'Fails with wrong type' {
+      $fake = [PSCustomObject]@{Name = 'Pester'; PSTypeName = 'NotATest' }
+      { Expand-TestCaseName -Test $fake } | Should -Throw '*is not a Pester Test or Pester Block*'
+    }
+    It 'Works with Array testcase' {
+      $baseMock.Name = 'Array TestCase <_>'
+      $baseMock.Data = @('pester')
+      Expand-TestCaseName $baseMock | Should -Be 'Array TestCase pester'
+    }
+    It 'Works with Single Hashtable testcase' {
+      $baseMock.Name = 'Array TestCase <Name>'
+      $baseMock.Data = @{Name = 'pester' }
+      Expand-TestCaseName $baseMock | Should -Be 'Array TestCase pester'
+    }
+    It 'Works with Multiple Hashtable testcase' {
+      $baseMock.Name = 'Array <Data> TestCase <Name>'
+      $baseMock.Data = @{Name = 'pester'; Data = 'aCoolTest' }
+      Expand-TestCaseName $baseMock | Should -Be 'Array aCoolTest TestCase pester'
+    }
 
     It 'Works with Pester.Block' {
-            $Block = Import-Clixml $Mocks/Block.clixml
-            Expand-TestCaseName $Block | Should -Be 'Describe Nested Foreach giraffe'
-        }
+      $Block = Import-Clixml $Mocks/Block.clixml
+      Expand-TestCaseName $Block | Should -Be 'Describe Nested Foreach giraffe'
     }
+  }
 
-    Context 'Get-DurationString' {
-        BeforeAll {
-            . $testScript 'fakepath' -LoadFunctionsOnly
-        }
-        Context 'When argument contains values for properties UserDuration and FrameworkDuration' {
-            It 'Should return the correct string' {
-                $mockTestParameter = [PSCustomObject] @{
-                    UserDuration = 10000
-                    FrameworkDuration = 20000
-                }
-                $getDurationStringResult = Get-DurationString $mockTestParameter
-                $getDurationStringResult | Should -Be '(1ms|2ms)'
-            }
-        }
-        Context 'When there are no value for property UserDuration' {
-            It 'Should return nothing' {
-                $mockTestParameter = [PSCustomObject] @{
-                    UserDuration = $null
-                    FrameworkDuration = 20000
-                }
-                $getDurationStringResult = Get-DurationString $mockTestParameter
-                $getDurationStringResult | Should -BeNullOrEmpty
-            }
-        }
-        Context 'When there are no value for property FrameworkDuration' {
-            It 'Should return nothing' {
-                $mockTestParameter = [PSCustomObject] @{
-                    UserDuration = 10000
-                    FrameworkDuration = $null
-                }
-                $getDurationStringResult = Get-DurationString $mockTestParameter
-                $getDurationStringResult | Should -BeNullOrEmpty
-            }
-        }
-        Context 'When there exist multiple Pester modules in the session' {
-            BeforeAll {
-                Mock -CommandName Get-Module -MockWith {
-                    return @('First','Second')
-                }
-            }
-            It 'Should throw an error' {
-                $mockTestParameter = [PSCustomObject] @{
-                    UserDuration = 10000
-                    FrameworkDuration = 20000
-                }
-                { Get-DurationString $mockTestParameter } | Should -Throw -ExpectedMessage '*Multiple Pester Modules found*'
-            }
-        }
+  Context 'Get-DurationString' {
+    BeforeAll {
+      . $testScript 'fakepath' -LoadFunctionsOnly
     }
+    Context 'When argument contains values for properties UserDuration and FrameworkDuration' {
+      It 'Should return the correct string' {
+        $mockTestParameter = [PSCustomObject] @{
+          UserDuration      = 10000
+          FrameworkDuration = 20000
+        }
+        $getDurationStringResult = Get-DurationString $mockTestParameter
+        $getDurationStringResult | Should -Be '(1ms|2ms)'
+      }
+    }
+    Context 'When there are no value for property UserDuration' {
+      It 'Should return nothing' {
+        $mockTestParameter = [PSCustomObject] @{
+          UserDuration      = $null
+          FrameworkDuration = 20000
+        }
+        $getDurationStringResult = Get-DurationString $mockTestParameter
+        $getDurationStringResult | Should -BeNullOrEmpty
+      }
+    }
+    Context 'When there are no value for property FrameworkDuration' {
+      It 'Should return nothing' {
+        $mockTestParameter = [PSCustomObject] @{
+          UserDuration      = 10000
+          FrameworkDuration = $null
+        }
+        $getDurationStringResult = Get-DurationString $mockTestParameter
+        $getDurationStringResult | Should -BeNullOrEmpty
+      }
+    }
+    Context 'When there exist multiple Pester modules in the session' {
+      BeforeAll {
+        Mock -CommandName Get-Module -MockWith {
+          return @('First', 'Second')
+        }
+      }
+      It 'Should throw an error' {
+        $mockTestParameter = [PSCustomObject] @{
+          UserDuration      = 10000
+          FrameworkDuration = 20000
+        }
+        { Get-DurationString $mockTestParameter } | Should -Throw -ExpectedMessage '*Multiple Pester Modules found*'
+      }
+    }
+  }
 }
